60a97d30e4a884fc873b1fe594c4907d520d2c59
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-08-18 00:21:38 +0300

    update

--- src/frontend/static/js/profile_dropdown.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const dropdownButton = document.querySelector('.relative.inline-block button');
-    const dropdownMenu = document.querySelector('.absolute.right-0');
-
-    dropdownButton.addEventListener('click', function () {
-        dropdownMenu.classList.toggle('hidden');
-    });
-});
